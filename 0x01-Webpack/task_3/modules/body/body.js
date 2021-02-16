ee8f1ebc8ace672dfc03f8cda2e70a16aedaf552
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2021-02-15 22:51:49 -0500

    Dev servers, modules, and tree shaking

---
+++ 0x01-Webpack/task_3/modules/body/body.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import './body.css';
+
+$('body').append(`<button>Click here to get started</button>`);
+$('body').append(`<p id='count'></p>`);
+
+function updateCounter() {
+  $(document).ready(function() {
+    let counter = $('#count').html()
+    $('button').click(function() {
+        counter++;
+        $('#count').html(`${counter} clicks on the button`);
+    });
+  });
+}
+
+_.debounce(updateCounter, 250);
+updateCounter();
