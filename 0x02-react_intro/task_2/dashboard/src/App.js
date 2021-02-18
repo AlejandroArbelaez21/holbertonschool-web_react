e9c7916d92d29f772226808aa0785b3f28a440e0
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2021-02-18 01:29:32 -0500

    Modify the App

---
+++ 0x02-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.jpg';
+import './App.css';
+import { getFooterCopy, getFullYear } from './utils'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className='App-body'>
+        <p>Login to access the full dashboard</p>
+        <label for="email">Email:</label>
+        <input type="email" name='email' id="email"></input>
+        <label for="password">Password:</label>
+        <input type="password" name='password' id="password"></input>
+        <button>Ok</button>
+      </body>
+      <footer className='App-footer'>
+        <p>Copyright { getFullYear() } - { getFooterCopy() } </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
